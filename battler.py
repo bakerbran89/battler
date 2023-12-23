557ea14fbe8693011694fb2012e16d194d5268ad
bbaker <fake@fakemail.com>
2023-12-22 21:48:54 -0800

    Changes to be committed: 	modified:   battler.py

--- battler.py
+++ battler.py
@@ -1,34 +1,38 @@
-# Initial parameters
+import time
 
-#characters = ['warewolf', 'robot', 'martian', 'velociraptor']
-userHealth = 100
-opponentHealth = 100
+# Character stats
+Robot={"attack":45, "defense":60, "illusive":50}
+Martian={"attack":45, "defense":60, "illusive":50}
+Velociraptor={"attack":45, "defense":60, "illusive":50}
+Warewolf={"attack":45, "defense":60, "illusive":50}
+# Characters
+characters=['Robot', 'Martian', 'Velociraptor', 'Warewolf']
+
+# Game functions
+'''def charSelect(val):
+  if val.isdigit():
+    print('digit')
+  elif val == 'R' or 'r':
+    print('R')
+  elif val == 'M' or 'm':
+    print('M')
+  elif val == 'V' or 'v':
+    print('V')
+  else:
+    print('W')
+
+  selected = characters[val]
+  print(selected)
+  return selected
+  '''
+
 
 # Player select
 promptUser = input("Select a character: 1) Robot 2) Martian 3) Velociraptor: ")
+charSelect(promptUser)
 
-#place the conditional into a function 
-if promptUser.lower() == 'robot' or promptUser[0].lower() == 'r' or promptUser == '1':
-    user='robot'
-    attack=45
-    defense=60
-    illusive=50
-    print("Robot")
-elif promptUser.lower() == 'martian' or promptUser[0].lower() == 'm' or promptUser == '2':
-    user='Martian'
-    attack=45
-    defense=60
-    illusive=50
-    print("Martian")
-elif promptUser.lower() == 'velociraptor' or promptUser[0].lower() == 'v' or promptUser == '3':
-    user='Velociraptor'
-    attack=45
-    defense=60
-    illusive=50
-    print("Velociraptor")
-else:
-    user='Warewolf'
-    attack=45
-    defense=60
-    illusive=50
-    print("Warewolf")
+# Game setup
+userHealth = 100
+opponentHealth = 100
+# Begin game loop
+#while (userHealth>0 and opponentHealth>0):
